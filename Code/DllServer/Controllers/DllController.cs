510d8b75e01fbbe396d1a55d9608b9d1bd230eaa
Antheius <cs22m023@technikum-wien.at>
2023-04-04 17:30:01 +0200

    Initial commit setup project

---
+++ Code/DllServer/Controllers/DllController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Reflection;
+
+namespace DllServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DllController : ControllerBase
+    {
+        [HttpGet("load")]
+        public async Task<IActionResult> LoadDll()
+        {
+            var asm = Assembly.Load("OurLibrary.dll");
+           
+            return Content(JsonConvert.SerializeObject(asm));
+        }
+    }
+}
