4d244049dc919bf1e6659c14ee25a7080ecb5ecb
Antheius <cs22m023@technikum-wien.at>
2023-04-12 20:46:39 +0200

    Finished demo including integrity verification

--- Code/Demo_Client_Windows/MainWindowViewModel.cs
+++ Code/Demo_Client_Windows/MainWindowViewModel.cs
@@ -15,6 +15,7 @@ namespace Demo_Client_Windows
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        private bool isIntegrityVerificationEnabled;
         private HttpClient httpClient;
 
         public MainWindowViewModel()
@@ -28,6 +29,20 @@ namespace Demo_Client_Windows
             private set;
         }
 
+        public bool IsIntegrityVerificationEnabled
+        {
+            get
+            {
+                return this.isIntegrityVerificationEnabled;
+            }
+
+            set
+            {
+                this.isIntegrityVerificationEnabled = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsIntegrityVerificationEnabled)));
+            }
+        }
+
         public ICommand LoadDllCommand
         {
             get
@@ -36,6 +51,22 @@ namespace Demo_Client_Windows
                 {
                     var response = await httpClient.GetAsync("https://localhost:5001/api/Dll/load");
                     var assemblyBytes = await response.Content.ReadAsByteArrayAsync();
+
+                    if (isIntegrityVerificationEnabled)
+                    {
+                        var hash = ComputeHash(assemblyBytes);
+
+                        if (hash != 1118940902)
+                        {
+                            var result = MessageBox.Show("Warning, the calculated hash for the loaded assembly did not match the expected hash value of \"1118940902\". Please ensure that you trust the assembly before proceeding, otherwise terminate the execution of this program. Press OK to continue despite this warning, at your own risk of course ;)", "Warning, file hashes do not match", MessageBoxButton.OKCancel, MessageBoxImage.Warning, MessageBoxResult.Cancel);
+                       
+                            if (result != MessageBoxResult.OK)
+                            {
+                                return;
+                            }
+                        }
+                    }
+
                     var assembly = Assembly.Load(assemblyBytes);
 
                     var control = assembly.GetTypes().FirstOrDefault(t => t.IsAssignableTo(typeof(UserControl)));
@@ -51,5 +82,24 @@ namespace Demo_Client_Windows
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        private int ComputeHash(params byte[] data)
+        {
+            unchecked
+            {
+                const int p = 16777619;
+                int hash = (int)2166136261;
+
+                for (int i = 0; i < data.Length; i++)
+                    hash = (hash ^ data[i]) * p;
+
+                hash += hash << 13;
+                hash ^= hash >> 7;
+                hash += hash << 3;
+                hash ^= hash >> 17;
+                hash += hash << 5;
+                return hash;
+            }
+        }
     }
 }
--- Code/DllServer/Controllers/DllController.cs
+++ Code/DllServer/Controllers/DllController.cs
@@ -10,10 +10,30 @@ namespace DllServer.Controllers
     [ApiController]
     public class DllController : ControllerBase
     {
+        private static string path;
+
+        static DllController()
+        {
+            path = @"C:\Users\Grego\source\repos\Software_Integrity_Failure_Demo\Code\WPFTools\bin\Debug\net6.0-windows\WPFTools.dll";
+        }
+
         [HttpGet("load")]
         public async Task<IActionResult> LoadDll()
         {
-            return File(System.IO.File.ReadAllBytes(@"C:\Users\Grego\source\repos\Software_Integrity_Failure_Demo\Code\MaliciousWPFTools\bin\Debug\net6.0-windows\MaliciousWPFTools.dll"), "application/octet-stream", "Ourlibrary.dll");
+            return File(System.IO.File.ReadAllBytes(path), "application/octet-stream", "Ourlibrary.dll");
+        }
+
+        [HttpGet("update")]
+        public async Task<IActionResult> UpdateDll(string password, string updatedPath)
+        {
+            if (password != "12345")
+            {
+                return Unauthorized();
+            }
+
+            path = updatedPath;
+
+            return Ok();
         }
     }
 }
